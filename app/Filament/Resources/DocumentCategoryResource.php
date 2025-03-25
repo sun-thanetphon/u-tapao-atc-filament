ac3a8d7fdaa6ce8978949c407a8e1a2ef010f049
thanetphonasn <168076384+thanetphonasn@users.noreply.github.com>
2025-03-25 16:50:13 +0700

    adjust group

--- app/Filament/Resources/DocumentCategoryResource.php
+++ app/Filament/Resources/DocumentCategoryResource.php
@@ -22,7 +22,7 @@ class DocumentCategoryResource extends Resource
 
     protected static ?int $navigationSort = 1;
 
-    protected static ?string $navigationGroup = 'Categories';
+    protected static ?string $navigationGroup = 'Documents management';
 
     public static function canAccess(): bool
     {
--- app/Filament/Resources/DocumentResource.php
+++ app/Filament/Resources/DocumentResource.php
@@ -24,7 +24,7 @@ class DocumentResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = 2;
 
     protected static ?string $navigationGroup = 'Documents management';
 
