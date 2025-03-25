a8595288ebe0f746a74f63e2119f2ec642d53ea1
thanetphonasn <168076384+thanetphonasn@users.noreply.github.com>
2025-03-25 15:57:41 +0700

    adjust css

--- resources/views/filament/pages/home.blade.php
+++ resources/views/filament/pages/home.blade.php
@@ -1,12 +1,13 @@
 <x-filament-panels::page>
 
-    <div class="flex">
+    <div class="flex flex-col md:flex-row">
         <div class="flex-1 p-4">
-            <img src="{{ secure_asset('assets/home/IMG_A3D5B332BB77-1.jpeg') }}" alt="Image 2" class="w-full h-auto">
+            <img src="{{ asset('assets/home/IMG_A3D5B332BB77-1.jpeg') }}" alt="Image 2" class="w-full h-auto">
         </div>
         <div class="flex-1 p-4">
-            <img src="{{ secure_asset('assets/home/IMG_0E0CBBC7CE7B-1.jpeg') }}" alt="Image 1" class="w-full h-auto">
+            <img src="{{ asset('assets/home/IMG_0E0CBBC7CE7B-1.jpeg') }}" alt="Image 1" class="w-full h-auto">
         </div>
     </div>
+
 
 </x-filament-panels::page>
