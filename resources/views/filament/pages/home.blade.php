5d75b1c8334f28ae41a308fa27104eeb8b35440e
thanetphonasn <168076384+thanetphonasn@users.noreply.github.com>
2025-03-25 15:53:05 +0700

    add secure link

--- resources/views/filament/pages/home.blade.php
+++ resources/views/filament/pages/home.blade.php
@@ -2,10 +2,10 @@
 
     <div class="flex">
         <div class="flex-1 p-4">
-            <img src="{{ asset('assets/home/IMG_A3D5B332BB77-1.jpeg') }}" alt="Image 2" class="w-full h-auto">
+            <img src="{{ secure_asset('assets/home/IMG_A3D5B332BB77-1.jpeg') }}" alt="Image 2" class="w-full h-auto">
         </div>
         <div class="flex-1 p-4">
-            <img src="{{ asset('assets/home/IMG_0E0CBBC7CE7B-1.jpeg') }}" alt="Image 1" class="w-full h-auto">
+            <img src="{{ secure_asset('assets/home/IMG_0E0CBBC7CE7B-1.jpeg') }}" alt="Image 1" class="w-full h-auto">
         </div>
     </div>
 
