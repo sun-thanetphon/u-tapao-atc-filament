4ab460c3a0fb155293def4adabb506b0e0b50b1b
Sunny <sun1996ct@gmail.com>
2025-04-05 15:22:10 +0700

    update

--- resources/views/filament/pages/home.blade.php
+++ resources/views/filament/pages/home.blade.php
@@ -6,7 +6,7 @@
                 class="w-full h-full object-cover">
         </div>
         <div class="flex-1 p-4">
-            <img src="{{ asset('assets/home/IMG_0E0CBBC7CE7B-1.jpeg') }}" alt="Image 1"
+            <img src="{{ asset('assets/home/IMG_0E0CBBC7CE7B-2.jpeg') }}" alt="Image 1"
                 class="w-full h-full object-cover">
         </div>
     </div>
