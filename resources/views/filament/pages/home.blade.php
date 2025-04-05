bf8d03dae5840f843419f3188761a86f93d76936
Sunny <sun1996ct@gmail.com>
2025-04-05 14:45:22 +0700

    Update home.blade.php

--- resources/views/filament/pages/home.blade.php
+++ resources/views/filament/pages/home.blade.php
@@ -2,10 +2,12 @@
 
     <div class="flex flex-col md:flex-row">
         <div class="flex-1 p-4">
-            <img src="{{ asset('assets/home/IMG_A3D5B332BB77-1.jpeg') }}" alt="Image 2" class="w-full h-auto">
+            <img src="{{ asset('assets/home/IMG_A3D5B332BB77-1.jpeg') }}" alt="Image 2"
+                class="w-full h-full object-cover">
         </div>
         <div class="flex-1 p-4">
-            <img src="{{ asset('assets/home/IMG_0E0CBBC7CE7B-1.jpeg') }}" alt="Image 1" class="w-full h-auto">
+            <img src="{{ asset('assets/home/IMG_0E0CBBC7CE7B-1.jpeg') }}" alt="Image 1"
+                class="w-full h-full object-cover">
         </div>
     </div>
 
