215d0edc11b969fef0bdbd8afd02465912ec6e4e
sunny-esper <>
2026-05-09 23:10:35 -0700

    edit wording

--- resources/views/filament/pages/service.blade.php
+++ resources/views/filament/pages/service.blade.php
@@ -1,7 +1,7 @@
 <x-filament-panels::page>
 
     <div class="p-6 rounded-lg borde">
-            <h2 class="text-xl font-medium">คำแนะนำการใช้บริการ</h2>
+        <h2 class="text-xl font-medium">คำแนะนำการใช้บริการ</h2>
         <p class="mt-4 font-sans">
             ระบบจัดเก็บและติดตามการรับทราบเอกสารนี้ จัดทำขึ้นเพื่อประโยชน์ต่อทางราชการ และเพื่อเพิ่มประสิทธิภาพในการปฏิบัติงานภายในหน่วยงาน โดยมุ่งแก้ไขปัญหาในการจัดเก็บ ค้นหา และติดตามการรับทราบเอกสารให้มีความสะดวก รวดเร็ว ถูกต้อง และเป็นระบบมากยิ่งขึ้น
         </p>
@@ -10,14 +10,20 @@
         </p>
 
         <h3 class="text-lg font-medium mt-6">ผู้จัดทำและผู้พัฒนา</h3>
-        <p class="mt-2 font-sans">
-            พ.จ.อ.บัลลังก์ บัลลังก์โพธิ์
-        </p>
+        <ul class="list-disc pl-6 mt-2 font-sans">
+            <li>พ.จ.อ.บัลลังก์ บัลลังก์โพธิ์</li>
+        </ul>
+
+        <h3 class="text-lg font-medium mt-6">ดูแลระบบและสนับสนุนโดย</h3>
+        <ul class="list-disc pl-6 mt-2 font-sans">
+            <li>ร.ท.ลิขิต ร่วมสุข</li>
+            <li>น.ต.ทองวิทย์ อิ้งชัยภูมิ</li>
+        </ul>
 
         <h3 class="text-lg font-medium mt-6">ติดต่อ</h3>
-        <ul class="list-disc pl-6 mt-2 font-sans">
-            <li><strong>อีเมล:</strong> <a href="mailto:keaw0022@gmail.com" class="text-blue-600 hover:underline">keaw0022@gmail.com</a></li>
-        </ul>
+        <p class="mt-2 font-sans">
+            อีเมล: keaw0022@gmail.com
+        </p>
     </div>
 
 </x-filament-panels::page>
